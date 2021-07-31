78d5c742362d2b4946859df201e6e44d1f713529
mason457 <88136451+mason457@users.noreply.github.com>
2021-07-31 19:14:15 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace _11101
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelx = new System.Windows.Forms.Label();
+            this.labely = new System.Windows.Forms.Label();
+            this.labelr = new System.Windows.Forms.Label();
+            this.labelg = new System.Windows.Forms.Label();
+            this.labelb = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOAD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelx
+            // 
+            this.labelx.AllowDrop = true;
+            this.labelx.AutoSize = true;
+            this.labelx.Location = new System.Drawing.Point(28, 191);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(22, 12);
+            this.labelx.TabIndex = 1;
+            this.labelx.Text = "X =";
+            // 
+            // labely
+            // 
+            this.labely.AllowDrop = true;
+            this.labely.AutoSize = true;
+            this.labely.Location = new System.Drawing.Point(28, 221);
+            this.labely.Name = "labely";
+            this.labely.Size = new System.Drawing.Size(22, 12);
+            this.labely.TabIndex = 2;
+            this.labely.Text = "Y =";
+            // 
+            // labelr
+            // 
+            this.labelr.AllowDrop = true;
+            this.labelr.AutoSize = true;
+            this.labelr.Location = new System.Drawing.Point(112, 191);
+            this.labelr.Name = "labelr";
+            this.labelr.Size = new System.Drawing.Size(22, 12);
+            this.labelr.TabIndex = 3;
+            this.labelr.Text = "R =";
+            // 
+            // labelg
+            // 
+            this.labelg.AllowDrop = true;
+            this.labelg.AutoSize = true;
+            this.labelg.Location = new System.Drawing.Point(112, 223);
+            this.labelg.Name = "labelg";
+            this.labelg.Size = new System.Drawing.Size(22, 12);
+            this.labelg.TabIndex = 4;
+            this.labelg.Text = "G =";
+            // 
+            // labelb
+            // 
+            this.labelb.AllowDrop = true;
+            this.labelb.AutoSize = true;
+            this.labelb.Location = new System.Drawing.Point(112, 257);
+            this.labelb.Name = "labelb";
+            this.labelb.Size = new System.Drawing.Size(22, 12);
+            this.labelb.TabIndex = 5;
+            this.labelb.Text = "B =";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(219, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "座標值";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "RGB值";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "SAVE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Invert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(517, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 414);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelb);
+            this.Controls.Add(this.labelg);
+            this.Controls.Add(this.labelr);
+            this.Controls.Add(this.labely);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelx;
+        private System.Windows.Forms.Label labely;
+        private System.Windows.Forms.Label labelr;
+        private System.Windows.Forms.Label labelg;
+        private System.Windows.Forms.Label labelb;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace _11101
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Bitmap bmp1;
+        Bitmap newbmp; 
+        int[,] img;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bmp = new Bitmap(openFileDialog1.FileName);                //圖片像素資料存於變數bmp
+                pictureBox1.Image = bmp;                                   //顯示於pictureBox1.
+                img = test_a.BmpToAry.Transfer(bmp);                         //將相速資料置入test.BmpToAry.Transfer函式，輸出陣列img            
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                newbmp.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);   //圖檔newbmp輸出
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1 != null)
+            {
+                int HEIGHT = img.GetLength(0);
+                int WIDTH = img.GetLength(1);
+                for (int i = 0; i < HEIGHT; i++)
+                {
+                    for (int j = 0; j < WIDTH; j++)
+                    {
+                        img[i, j] = 255-img[i, j];
+                    }
+                } 
+                newbmp = test_a.BmpToAry.Invert(img);
+                pictureBox2.Image = newbmp;
+            }
+            else
+            {
+                MessageBox.Show("請先載入圖形");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Timer, 每10ms讀取滑鼠座標值及像素顏色並顯示於 PictureBox背景
+            Color color = GetPixelColor(Cursor.Position);
+            labelx.Text = "X = " + Cursor.Position.X.ToString();
+            labely.Text = "Y = " + Cursor.Position.Y.ToString();
+            labelr.Text = " R = " + color.R;
+            labelg.Text = " G = " + color.G;
+            labelb.Text = " B = " + color.B;
+            pictureBox1.BackColor = color;
+        }
+        static Color GetPixelColor(Point position)
+        {
+            using (var bitmap = new Bitmap(1, 1))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.CopyFromScreen(position, new Point(0, 0), new Size(1, 1));
+                }
+                return bitmap.GetPixel(0, 0);
+            }
+        }
+
+        private void labela_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
